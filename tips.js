e84549c2e2576ed1011e0951627b0b8975e4f174
mollybeach <mollyjbeach@gmail.com>
2021-11-13 15:44:20 -0500

    mint before deploy

--- tips.js
+++ tips.js
@@ -1,7 +1,21 @@
 `
 1. FORK_MODE=true npx hardhat node 
 2. Open another terminal and navigate to the stacked toads directory 
-Npx hardhat Npx hardhat deploy-default-erc20 --network rinkby
-Npx hardhat deploy-lottery --network rinkby
-    
+Npx hardhat deploy-default-20 --network rinkbey
+Npx hardhat deploy-lottery --network rinkbey
+
+
+yarn run hardhat deploy-default-20 --network localhost
+//mint before deploy lottery
+
+yarn run hardhat mint-default-20 —network localhost
+yarn run hardhat deploy-lottery --network localhost
+
+
+
+Error HH700: Artifact for contract "DefaultErc20" not found : 
+yarn run hardhat compile 
+
+Deployed contract DefaultErc20 to: 0x81a52516768C42C1555ed8B5F4BC457F03AEB4e6
+
 `
