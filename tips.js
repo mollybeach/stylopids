510bfcbabbbcc951263697aebc2a7f3f9240d343
mollybeach <mollyjbeach@gmail.com>
2021-11-13 15:02:08 -0500

    added first tips

---
+++ tips.js
@@ -0,0 +1,7 @@
+`
+1. FORK_MODE=true npx hardhat node 
+2. Open another terminal and navigate to the stacked toads directory 
+Npx hardhat Npx hardhat deploy-default-erc20 --network rinkby
+Npx hardhat deploy-lottery --network rinkby
+    
+`
